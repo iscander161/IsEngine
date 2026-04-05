488d01e25250fdbe0851971d5b28bfd7dc98cfa6
Iscander <iscander161@yandex.ru>
2026-04-05 21:18:13 +0300

    The simplest application

---
+++ Tools/IsMusicEditor/Main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Main.cpp
+ * @author Alexander Tolstunov (iscander161@yandex.ru)
+ * @brief 
+ * @version 0.1
+ * @date 05.04.2026
+ * @copyright Copyright (c) 2026
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+
+/**
+ * @brief Application main entry point.
+ * @param argc - Number of application arguments.
+ * @param argvs - Application arguments.
+ * @return int - Result of application.
+ */
+int main(int argc, char** argvs) {
+    int appResult = EXIT_SUCCESS;
+    return appResult;
+}
